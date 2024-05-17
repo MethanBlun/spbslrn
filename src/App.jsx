0bdad1161b720f9223fa7e3238fa922f2dd46ec4
Osirus Jack <methanblun@gmail.com>
2024-05-17 11:41:26 +0000

    try to setup supabase

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-import { useEffect, useState } from "react";
-import { createClient } from "@supabase/supabase-js";
 import React from "react";
-const supabase = createClient("https://wrsgdyacajazvpomkcyu.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indyc2dkeWFjYWphenZwb21rY3l1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUxODQzNzIsImV4cCI6MjAzMDc2MDM3Mn0.Z8aA6fY06IljS_fHlQ6_of1CpG0eqN0nTdkA6xPngvs");
-
+// import supabase from "./config/supabaseClient";
+alert('hello world')
 function App() {
-  const [countries, setCountries] = useState([]);
+  // console.log(supabase)
+  return <h1>hello worlds</h1>
+    
 
-  useEffect(() => {
-    getCountries();
-  }, []);
 
-  async function getCountries() {
-    const { data } = await supabase.from("countries").select();
-    setCountries(data);
-  }
-
-  return (
-    <ul>
-      {countries.map((country) => (
-        <li key={country.name}>{country.name}</li>
-      ))}
-    </ul>
-  );
 }
 
 export default App;
---
+++ src/config/supabaseClient.jsx
@@ -0,0 +1,29 @@
+// import { createClient } from "@supabase/supabase-js";
+
+// const supabaseUrl = process.meta.REACT_APP_SUPABASE_URL;
+// const supabaseKey = process.meta.REACT_APP_SUPABASE_KEY;
+
+// if (!supabaseUrl || !supabaseKey) {
+//   throw new Error("Missing Supabase URL or Key environment variables");
+// }
+
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
+// export default supabase;
+
+// import { createClient } from "@supabase/supabase-js";
+
+// const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+// const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
+// export default supabase;
+
+
+// import { createClient } from "@supabase/supabase-js";
+
+// const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+// const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+// const supabase = createClient(supabaseUrl, supabaseKey);
+
+// export default supabase;
