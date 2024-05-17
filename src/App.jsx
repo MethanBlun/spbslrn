975ad965bf461a6a88d5116fa35b0d44075237d4
Osirus Jack <methanblun@gmail.com>
2024-05-17 18:55:27 +0000

    setup al things according to searches

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
 import React from "react";
 import supabase from "./config/supabaseClient";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [fetchError, setFetchError] = useState(null);
+  const [task, setTask] = useState(null);
 
-  return <h1>hello worlds</h1>
-    
+  useEffect(() => {
+    const fetchQuotes = async () => {
+      const { data, error } = await supabase.from("learning Table").select;
+
+      if (error) {
+        setFetchError(console.log("could not fetch task"));
+        setTask(null);
+      }
+      if (data) {
+        setTask(data);
+        setTask(null);
+      }
+    };
 
 
+    fetchQuotes()
+
+  }, []);
+  return (
+    <div > 
+      {fetchError }
+    </div>
+  );
 }
 
 export default App;
--- src/config/supabaseClient.jsx
+++ src/config/supabaseClient.jsx
@@ -3,9 +3,6 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = import.meta.env.VITE_APP_URL;
 const supabaseKey = import.meta.env.VITE_APP_ANON_KEY;
 
-console.log('Supabase URL:', supabaseUrl); 
-console.log('Supabase Key:', supabaseKey); 
-
 if (!supabaseUrl || !supabaseKey) {
     throw new Error('Supabase URL and Key are required');
 }
